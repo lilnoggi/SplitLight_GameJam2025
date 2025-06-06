2d55c6939ad7c7465dc6a9a2559d4dd83cdd0be4
Abodikt <BenediktGermann@outlook.de>
2025-06-06 02:46:28 +0200

    Added "DIALOG" as a Game State

--- Assets/Scripts/GM.gd
+++ Assets/Scripts/GM.gd
@@ -1,7 +1,7 @@
 extends Node
 
 # the main gamestates
-enum GAMESTATE {MENU, PLAY, PAUSE}
+enum GAMESTATE {MENU, PLAY, PAUSE, DIALOG}
 
 
 var current_state: GAMESTATE
@@ -10,6 +10,7 @@ var current_state: GAMESTATE
 func _ready():
 	current_state = GAMESTATE.MENU
 
+
 func change_state(new_state: GAMESTATE):
 	match new_state:
 		GAMESTATE.MENU:
@@ -17,6 +18,8 @@ func change_state(new_state: GAMESTATE):
 		GAMESTATE.PLAY:
 			Engine.time_scale = 1
 		GAMESTATE.PAUSE:
+			Engine.time_scale = 0
+		GAMESTATE.DIALOG:
 			Engine.time_scale = 0
 
 
